fedab0c9c48c8520fe5af330cf82753b1d91bdc1
johann2357 <johann2357@gmail.com>
2013-11-16 18:22:04 -0500

    simulation using both distributions

--- thread-for/main.cpp
+++ thread-for/main.cpp
@@ -4,22 +4,48 @@
 
 struct Process
 {
-  void operator() (char* it)
+  void operator() (int* it)
   {
-    Debug(*it);
+    int x = 1;
+    for (int i = 0; i < 9999; ++i)
+      for (int j = 0; j < 999; ++j)
+        ++x;
   }
 };
 
+struct Process2
+{
+  void operator() (int* it)
+  {
+    int x = 1;
+    for (int i = 0; i < 999; ++i)
+      for (int j = 0; j < ((*it)*9); ++j)
+        ++x;
+  }
+};
+
+void test_dist1(std::vector<int>* ctnr)
+{
+  Debug("test dist 1\n");
+  tfor< Distribution<std::vector<int>, Process> > test;
+  test(ctnr);
+}
+
+void test_dist2(std::vector<int>* ctnr)
+{
+  Debug("test dist 2\n");
+  tfor< Distribution2<std::vector<int>, Process2> > test;
+  test(ctnr);
+}
+
 int main()
 {
-  std::vector<char>* ctnr = NULL;
-  ctnr = new std::vector<char>({'a','b','c','d','e','f','g','h'});
-  Debug("test\n");
-  tfor< Distribution<std::vector<char>, Process> > test;
-  test(ctnr);
-  Debug("\ntest2\n");
-  tfor< Distribution2<std::vector<char>, Process> > test2;
-  test2(ctnr);
+  std::vector<int>* ctnr = NULL;
+  ctnr = new std::vector<int>;
+  for (int i = 0; i < 999; ++i)
+    ctnr->push_back((i));
+  test_dist1(ctnr);
+  //test_dist2(ctnr);
   delete ctnr;
   Debug("\nthe end\n");
   return 0;

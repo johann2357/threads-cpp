048932e8fd4cb25f321f74efb578a684027dadaa
johann2357 <johann2357@gmail.com>
2013-11-20 12:40:10 -0500

    improving tests

--- thread-for/main.cpp
+++ thread-for/main.cpp
@@ -1,52 +1,75 @@
-#include <vector>
+#include <fstream>
+#include <list>
 #include "tfor.h"
 #include "debug.h"
+
+#define MAX_MOD 1000
 
 struct Process
 {
   void operator() (int* it)
   {
     int x = 1;
-    for (int i = 0; i < 9999; ++i)
-      for (int j = 0; j < 999; ++j)
-        ++x;
+    for (int i = 0; i < MAX_MOD; ++i)
+      ++x;
   }
 };
 
 struct Process2
 {
-  void operator() (int* it)
+  void operator() (int* val)
   {
-    int x = 1;
-    for (int i = 0; i < 999; ++i)
-      for (int j = 0; j < ((*it)*9); ++j)
-        ++x;
+    long long sum = 0;
+    for (int i = 1; i <= (*val)%MAX_MOD; i++)
+       sum += (*val) % i;
   }
 };
 
-void test_dist1(std::vector<int>* ctnr)
+void test_dist1(std::list<int>* ctnr)
 {
   Debug("test dist 1\n");
-  tfor< Distribution<std::vector<int>, Process> > test;
+  using namespace std;
+  chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+  //
+  tfor< Distribution<std::list<int>, Process2> > test;
   test(ctnr);
+  //
+  chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+  Debug("It took me ") ;
+  Debug(chrono::duration_cast<chrono::nanoseconds>(t2-t1).count()*0.000000001);
+  Debug(" seconds.\n");
 }
 
-void test_dist2(std::vector<int>* ctnr)
+void test_dist2(std::list<int>* ctnr)
 {
   Debug("test dist 2\n");
-  tfor< Distribution2<std::vector<int>, Process2> > test;
+  using namespace std;
+  chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+  //
+  tfor< Distribution2<std::list<int>, Process2> > test;
   test(ctnr);
+  //
+  chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+  Debug("It took me ") ;
+  Debug(chrono::duration_cast<chrono::nanoseconds>(t2-t1).count()*0.000000001);
+  Debug(" seconds.\n");
 }
 
 int main()
 {
-  std::vector<int>* ctnr = NULL;
-  ctnr = new std::vector<int>;
-  for (int i = 0; i < 999; ++i)
-    ctnr->push_back((i));
-  test_dist1(ctnr);
-  //test_dist2(ctnr);
-  delete ctnr;
+  Debug("loading data_long.dat ... ");
+  std::ifstream fin("data_long.dat");
+  std::list<int> data;
+  while(!fin.eof())
+  {
+    int val;
+    fin>>val;
+    data.push_back(val);
+  }
+  Debug("done\n");
+  Debug("MAX_MOD = " << MAX_MOD << "\n");
+  test_dist1(&data);
+  test_dist2(&data);
   Debug("\nthe end\n");
   return 0;
 }
